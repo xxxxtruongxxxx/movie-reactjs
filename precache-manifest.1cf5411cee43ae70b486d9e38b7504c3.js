e89285270864af4df09755eefd25c9b521296b12
Hoàng Trương <hoangxuantruong208@gmail.com>
2022-12-27 12:17:59 +0700

    Updates

---
+++ precache-manifest.1cf5411cee43ae70b486d9e38b7504c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10a5a9fd45be3002bcc1e2eaa07de2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1855345777d74a7ad08e",
+    "url": "./static/css/main.944439c0.chunk.css"
+  },
+  {
+    "revision": "2bfdbe806dac3d3ebe8e",
+    "url": "./static/js/2.38038f42.chunk.js"
+  },
+  {
+    "revision": "0c7bbd83885d4088b0424e023fce20ad",
+    "url": "./static/js/2.38038f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1855345777d74a7ad08e",
+    "url": "./static/js/main.baeae70e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.baeae70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc9fd83e633ebc6d38d",
+    "url": "./static/js/runtime-main.74b10c79.js"
+  }
+]);
